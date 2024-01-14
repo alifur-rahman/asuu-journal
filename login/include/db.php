40e089c81ed38dcf6c7aefd083750349edb05802
Alifur Rahman <alifurrahman1999@gmail.com>
2024-01-14 21:41:50 +0600

    init files

---
+++ login/include/db.php
@@ -0,0 +1,7 @@
+<?php
+$db = mysqli_connect("localhost", "root", "", "asuu_database");
+// Check connection
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+?>
